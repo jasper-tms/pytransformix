d86a7b2ddc61aa4d0408b2d4919786fc9662055e
Jasper Phelps <jtmaniatesselvin@g.harvard.edu>
2021-09-04 15:48:35 -0400

    Add package setup files and transformix.transform_points function
    I had python transformix wrappers running around in 3 different repos
    (htem/GridTape_VNC_paper, htem/FANC_auto_recon, and htem/run_elastix) so
    I'm pulling them out to a common place to prevent them from diverging,
    which they were doing. The version here is a slight extension of the
    version of those three that had the most features, which was the one in
    the run_elastix repo. Soon I'll make those other repos import the
    function here instead of having their own version of it.

---
+++ setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+with open('requirements.txt', 'r') as f:
+    requirements = f.read().splitlines()
+    requirements = [l for l in requirements if not l.startswith('#')]
+
+setuptools.setup(
+    name='transformix',
+    version='0.2',
+    author='Jasper Phelps',
+    author_email='jtmaniatesselvin@g.harvard.edu',
+    description='Python wrappers for transformix',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/jasper-tms/pytransformix',
+    license='GNU GPL v3',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+
+        'Operating System :: MacOS',
+        'Operating System :: POSIX :: Linux',
+    ],
+    python_requires='>=3.6',
+    install_requires=requirements
+)
---
+++ transformix/__init__.py
@@ -0,0 +1,1 @@
+from .core import transform_points
